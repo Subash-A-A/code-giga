09f6e0b440b82667ad028ae63bc14fa510ecc546
Subash <20i301@psgtech.ac.in>
2023-05-22 17:12:33 +0530

    animations, particles and wall checks

--- Code Giga/Assets/Scripts/PlayerTestMovement.cs
+++ Code Giga/Assets/Scripts/PlayerTestMovement.cs
@@ -3,39 +3,63 @@ using UnityEngine;
 public class PlayerTestMovement : MonoBehaviour
 {
     [SerializeField] private float _yPos = 0.5f;
+    [SerializeField] private GameObject _splashEffect;
     private Vector3 _pos;
+    private Animator _animator;
+    private WallCheck _check;
 
     private void Start()
     {
         _pos = new Vector3(0, _yPos, 0);
+        _animator = GetComponent<Animator>();
+        _check = GetComponent<WallCheck>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            _pos += new Vector3(0, 0, 1);
-        }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            _pos += new Vector3(0, 0, -1);
-        }
 
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            _pos += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            _pos += new Vector3(1, 0, 0);
-        }
-
+        TestMove();
         UpdatePosition();
     }
 
     private void UpdatePosition()
     {
         transform.position = Vector3.Lerp(transform.position, _pos, 10 * Time.deltaTime);
+    }
+
+    private void TestMove()
+    {
+        if ((_check._wallCheck[0] && Input.GetKeyDown(KeyCode.U)) ||
+            (_check._wallCheck[1] && Input.GetKeyDown(KeyCode.J)) ||
+            (_check._wallCheck[2] && Input.GetKeyDown(KeyCode.H)) ||
+            (_check._wallCheck[3] && Input.GetKeyDown(KeyCode.K))
+            )
+        {
+            _animator.SetTrigger("Jump");
+            Instantiate(_splashEffect, transform.position, Quaternion.identity);
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.U) && !_check._wallCheck[0])
+        {
+            _pos += new Vector3(0, 0, 1);
+            _animator.SetTrigger("Jump");
+        }
+        if (Input.GetKeyDown(KeyCode.J) && !_check._wallCheck[1])
+        {
+            _pos += new Vector3(0, 0, -1);
+            _animator.SetTrigger("Jump");
+        }
+
+        if (Input.GetKeyDown(KeyCode.H) && !_check._wallCheck[2])
+        {
+            _pos += new Vector3(-1, 0, 0);
+            _animator.SetTrigger("Jump");
+        }
+        if (Input.GetKeyDown(KeyCode.K) && !_check._wallCheck[3])
+        {
+            _pos += new Vector3(1, 0, 0);
+            _animator.SetTrigger("Jump");
+        }
     }
     
 }
---
+++ Code Giga/Assets/Scripts/WallCheck.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class WallCheck : MonoBehaviour
+{
+    [SerializeField] private Transform[] _wallCheckSensors;
+    [SerializeField] private LayerMask _whatIsWall;
+    public bool[] _wallCheck;
+
+    private void Start()
+    {
+        _wallCheck = new bool[4];
+    }
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < _wallCheckSensors.Length; i++)
+        {
+            CheckForWalls(i);
+        }
+    }
+
+    private void CheckForWalls(int wallIndex)
+    {
+        if (Physics.CheckSphere(_wallCheckSensors[wallIndex].position, 0.1f, _whatIsWall))
+        {
+            _wallCheck[wallIndex] = true;
+        }
+        else
+        {
+            _wallCheck[wallIndex] = false;
+        }
+    }
+}
